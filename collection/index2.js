4ad37ef6541ab14263f930475d4768e50c9a8888
sjoerdbijlsb <s.bijl_1@tilburguniversity.edu>
2022-12-10 14:48:12 +0100

    add new api

---
+++ collection/index2.js
@@ -0,0 +1,46 @@
+const SneaksAPI = require('sneaks-api');
+const sneaks = new SneaksAPI();
+const fs = require('fs');
+
+const styleIDs = ["dz6755-100", "fb8825-111", "387324-01", "dc1975-005", "uxc72al"]; // removed the extra : character here
+
+//Product object includes styleID where you input it in the getProductPrices function
+//getProductPrices(styleID, callback) takes in a style ID and returns sneaker info including a price map and more images of the product
+function getProductPrices(styleID) {
+  sneaks.getProductPrices(styleID, function(err, product){
+    console.log(product);
+
+    // check if the file exists, and create an empty file if it doesn't
+    fs.access('data.json', fs.constants.F_OK, function (err) {
+        if (err) {
+          fs.writeFileSync('data.json', '{ "data": [] }');
+        }
+      });
+
+    // read the file and parse the JSON
+    fs.readFile('data.json', 'utf8', (err, fileData) => {
+      if (err) throw err;
+      let data = JSON.parse(fileData);
+
+      // append the product data to the file
+      data.data.push(product);
+
+      // write the updated data to the file
+      fs.writeFile('data.json', JSON.stringify(data), (err) => {
+        if (err) throw err;
+        console.log('Data saved to data.json file!');
+      });
+    });
+  });
+}
+
+// call setInterval() to run the function every 5 seconds
+const interval = setInterval(() => {
+  // call the getProductPrices function for each style ID
+  styleIDs.forEach(styleID => getProductPrices(styleID));
+}, 5000);
+
+// stop setInterval() from running after 10 seconds
+setTimeout(() => {
+  clearInterval(interval);
+}, 10000);
