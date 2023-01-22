426d13f8901b41cd7bbe62bf24b2af83f00b0157
Sjoerdbijlsb <s.bijl_1@tilburguniversity.edu>
2023-01-22 15:28:18 +0100

    wrote lines to analyse file

--- src/analysis/r_summary.R
+++ src/analysis/r_summary.R
@@ -24,19 +24,3 @@ df$release_date <- format(df$release_date, format = "%d-%m-%Y")
 head(df)
 View(df)
 
-df %>% sort(release_date)
-
-
-df2 <-read.csv("../../data/resale_transactions_ALL.csv")
-head(df2)
-
-df3 <-  read.csv("../../data/val_sneakers_df.csv")
-View(df3)
-
-df2 <- df2 %>% 
-  rename(pid = image_fileName) %>% 
-  full_join(df3, by = "pid")
-  sort
-
-
-View(df2)
