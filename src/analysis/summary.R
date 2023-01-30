c5469d7d73c899987b86c1bab1c88861bf9dd956
sjoerdbijlsb <s.bijl_1@tilburguniversity.edu>
2023-01-30 15:21:07 +0100

    changed name of summary file

--- src/analysis/summary.R
+++ src/analysis/summary.R
@@ -1,26 +1,19 @@
 # load tidyverse package
 library(tidyverse)
 library(stringr)
-library(lubridate)
 
-# load in csv file of scraped data
-df <- read_csv("../../data/sneaker_info.csv")
-
-#summarize
-summary(df)
+# load in csv file of scraped data from goat.com
+df <- read_csv("../../data/sneaker_info_update.csv")
 
 # delete exact duplicates
 df <- distinct(df)
 
 # remove unwanted signs from columnns
 df$product_name <- str_replace_all(df$product_name, "[\\[\\]']", "")
-df$product_name <- str_replace_all(df$product_name, '"' , "")
+df$product_name <- str_replace_all(df$product_name, '"', "")
 df$brand <- str_replace_all(df$brand, "[\\[\\]']", "")
 
 # change date format of release date column
-df$release_date <- as.Date(df$release_date, format = "%m-%d-%Y")
-df$release_date <- format(df$release_date, format = "%d-%m-%Y")
+df$release_date <- format(as.Date(df$release_date, format = "%m-%d-%Y"), format = "%d-%m-%Y") 
 
 head(df)
-View(df)
-
