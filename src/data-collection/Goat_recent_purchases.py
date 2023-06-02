bbb32cf87f92d2a0af132f5b91183a190328aabb
sjoerdbijlsb <s.bijl_1@tilburguniversity.edu>
2023-06-02 15:52:52 +0200

    added right scraper for recent purchases

--- src/data-collection/Goat_recent_purchases.py
+++ src/data-collection/Goat_recent_purchases.py
@@ -1,29 +1,38 @@
-import csv
 import logging
-import pandas as pd
+from bs4 import BeautifulSoup
 import time
+import json
+import csv
 from datetime import datetime
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
-import json
-import requests.exceptions
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
 
+# Set up logging
+logging.basicConfig(filename='error.log', level=logging.ERROR)
 
-# Define options for the webdriver
-options = webdriver.ChromeOptions()
+# Create the Chrome options object and add the desired capabilities
+# (we are using a webdriver at some point)
+options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-extensions")
-options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36')
 
 # Initialize the webdriver object
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 # Open the csv file and extract the slug column using list comprehension
-with open("../../data/all_products20230514.csv", mode="r", newline="", encoding="utf-8") as file:
+
+# Open the first CSV file and extract the slug column using list comprehension
+with open("../../data/recinfo_2023-06-01.csv", mode="r", newline="", encoding="utf-8") as file:
     reader = csv.DictReader(file)
-    slugs = [row["slug"] for row in reader]
+    slugs = [row["rec_slug"] for row in reader]
+
+# Open the second CSV file and extract the slug column using list comprehension
+with open("sample.csv", mode="r", newline="", encoding="utf-8") as file:
+    reader = csv.DictReader(file)
+    slugs.extend([row["slug"] for row in reader])
 
 # Remove duplicates from the list of slugs
 slugs = list(set(slugs))
@@ -36,10 +45,10 @@ df = pd.DataFrame(columns=header)
 
 # Loop through the slugs and scrape data for each row
 count = 0
-slugs_to_extract_again = []
 for slug in slugs:
     extraction_timestamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S') # Define extraction_timestamp here
     start_time = time.time()  # Define start_time here
+    time.sleep(0.05)
     url = f"https://www.goat.com/web-api/v1/offers-data/recent-purchases?slug={slug}"
     try:
         driver.get(url)
@@ -53,36 +62,15 @@ for slug in slugs:
     except (AttributeError, KeyError) as e:
         logging.error(f"Error processing ID {id}: {str(e)}")
         continue
-    except requests.exceptions.RequestException as e:
-        # If there's an internet error, wait for 10 seconds and then retry the request
-        logging.error(f"Internet error occurred. Waiting for 10 seconds before retrying.")
-        time.sleep(10)
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source, 'html5lib').pre.text
-        data = json.loads(soup.strip('<pre style="word-wrap: break-word; white-space: pre-wrap;">').strip('</pre>'))
-        if data is None:
-            raise ValueError("No data found")
+    except Exception as e:
+        logging.error(f"Unexpected error occurred: {str(e)}")
+        continue
 
-    # If there is no data for the current slug, add an empty row to the dataframe
+        # If there is no data for the current slug, add an empty row to the dataframe
     if "priceDataList" not in data or not data["priceDataList"]:
         row = [slug] + [""]*(len(header)-1)
         df.loc[len(df)] = row
     else:
-        # Check the number of rows in the priceDataList
-        num_rows = len(data["priceDataList"])
-        if num_rows == 20:
-            # Calculate the timespan between the first and last extraction
-            first_extr_seconds = data["priceDataList"][0]["purchasedAt"]["seconds"]
-            first_extr_nanos = data["priceDataList"][0]["purchasedAt"]["nanos"]
-            last_extr_seconds = data["priceDataList"][-1]["purchasedAt"]["seconds"]
-            last_extr_nanos = data["priceDataList"][-1]["purchasedAt"]["nanos"]
-            timespan = (last_extr_seconds - first_extr_seconds) + (last_extr_nanos - first_extr_nanos) / 1e9
-            
-            # Decide when to extract the data again for this slug
-            if timespan < 86400:  # If timespan is less than an day, extract again after an hour
-                # Add the slug to a list of slugs to be extracted again later
-                slugs_to_extract_again.append(slug)
-        
         # Loop through the priceDataList and add each row to the dataframe
         for item in data["priceDataList"]:
             row = [
@@ -103,88 +91,13 @@ for slug in slugs:
             df.loc[len(df)] = row
 
     count += 1
-    print(f"Iteration {count}/{len(slugs) + len(slugs_to_extract_again)} took {time.time()-start_time:.2f} seconds.")
+    print(f"Iteration {count}/{len(slugs)} took {time.time()-start_time:.2f} seconds.")
 
-while True:
-    try:
-        # Loop through the slugs to be extracted again
-        for slug in slugs_to_extract_again:
-            extraction_timestamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-            start_time = time.time()
-            url = f"https://www.goat.com/web-api/v1/offers-data/recent-purchases?slug={slug}"
-            try:
-                driver.get(url)
-                soup = BeautifulSoup(driver.page_source, 'html5lib').pre.text
-                data = json.loads(soup.strip('<pre style="word-wrap: break-word; white-space: pre-wrap;">').strip('</pre>'))
-                if data is None:
-                    raise ValueError("No data found")
-            except ValueError as e:
-                logging.error(f"Error processing ID {id}: {str(e)}")
-                continue
-            except (AttributeError, KeyError) as e:
-                logging.error(f"Error processing ID {id}: {str(e)}")
-                continue
-            except requests.exceptions.RequestException as e:
-                logging.error(f"Internet error occurred. Waiting for 10 seconds before retrying.")
-                time.sleep(10)
-                driver.get(url)
-                soup = BeautifulSoup(driver.page_source, 'html5lib').pre.text
-                data = json.loads(soup.strip('<pre style="word-wrap: break-word; white-space: pre-wrap;">').strip('</pre>'))
-                if data is None:
-                    raise ValueError("No data found")
+# Get the current date in YYYY-MM-DD format
+current_date = datetime.now().strftime("%Y-%m-%d")
+# Write the dataframe to a csv file with the current date appended to the file name
+df.to_csv(f"../../data/recent_purchases_{current_date}.csv", index=False)
 
-            if "priceDataList" not in data or not data["priceDataList"]:
-                row = [slug] + [""]*(len(header)-1)
-                df.loc[len(df)] = row
-            else:
-                for item in data["priceDataList"]:
-                    row = [
-                        slug,
-                        item.get("type"),
-                        item["priceCents"].get("currency"),
-                        item["priceCents"].get("amount"),
-                        item["priceCents"].get("amountUsdCents"),
-                        item.get("sizeUs"),
-                        item.get("location"),
-                        extraction_timestamp,
-                        item.get("purchasedAt", {}).get("seconds"),
-                        item.get("purchasedAt", {}).get("nanos"),
-                        item.get("productCondition"),
-                        item.get("sizeOption", {}).get("presentation"),
-                        item.get("sizeOption", {}).get("value")
-                    ]
-                    df.loc[len(df)] = row
-
-            count += 1
-            print(f"Iteration {count}/{len(slugs_to_extract_again)} took {time.time()-start_time:.2f} seconds.")
-
-        # Clear the list of slugs to be extracted again
-        slugs_to_extract_again.clear()
-
-        # Wait for a short period before extracting data again
-        time.sleep(20) # wait for an hour before re-extracting data
-
-    except KeyboardInterrupt:
-        current_date = datetime.now().strftime("%Y-%m-%d")
-        df.to_csv(f"../../data/recent_purchases_{current_date}.csv", index=False, mode='a')
-        print("Keyboard interrupt received. Data saved to CSV file.")
-        break
-
-    except Exception as e:
-        logging.error(f"Error occurred: {str(e)}")
-        continue
-
-    finally:
-        current_date = datetime.now().strftime("%Y-%m-%d")
-        import os
-        if not os.path.exists(f"../../data/recent_purchases_{current_date}.csv"):
-            # Write the header if the file does not exist
-            df.to_csv(f"../../data/recent_purchases_{current_date}.csv", index=False, header=True)
-        else:
-            # Append to the file without writing the header
-            df.to_csv(f"../../data/recent_purchases_{current_date}.csv", index=False, mode='a', header=False)
-    
-        print("Scraping done")
-        driver.quit()
-
+# Quit the webdriver
+driver.quit()
 
